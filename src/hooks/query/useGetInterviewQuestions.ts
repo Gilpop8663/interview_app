21d320793e7976b3df5b02928e19738e73e3291d
Gilpop8663 <wolfye@naver.com>
2025-03-13 14:13:00 +0900

    feat: 진행도 저장되도록 구현

--- src/hooks/query/useGetInterviewQuestions.ts
+++ src/hooks/query/useGetInterviewQuestions.ts
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { GET_INTERVIEW_QUESTIONS } from '../../gql/query/interview';
 import { useSuspenseQuery } from '@apollo/client';
 import { Alert } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface InterviewQuestion {
   id: number;
@@ -12,6 +13,24 @@ interface GetInterviewQuestionsData {
   getInterviewQuestions: InterviewQuestion[];
 }
 
+const saveProgress = async (questionIndex: number) => {
+  try {
+    await AsyncStorage.setItem('progress', JSON.stringify(questionIndex));
+  } catch (error) {
+    console.error('진행도 저장 실패:', error);
+  }
+};
+
+const loadProgress = async () => {
+  try {
+    const progress = await AsyncStorage.getItem('progress');
+    return progress ? JSON.parse(progress) : 0;
+  } catch (error) {
+    console.error('진행도 불러오기 실패:', error);
+    return 0;
+  }
+};
+
 export const useGetInterviewQuestions = () => {
   const { data, error } = useSuspenseQuery<GetInterviewQuestionsData>(
     GET_INTERVIEW_QUESTIONS
@@ -20,11 +39,20 @@ export const useGetInterviewQuestions = () => {
 
   const handleNextQuestion = () => {
     if (questionIndex < data.getInterviewQuestions.length - 1) {
-      setQuestionIndex(questionIndex + 1);
+      const newIndex = questionIndex + 1;
+      setQuestionIndex(newIndex);
+      saveProgress(newIndex);
     } else {
       Alert.alert('End of Interview', '면접이 종료되었습니다.');
     }
   };
+
+  useEffect(() => {
+    (async () => {
+      const savedProgress = await loadProgress();
+      setQuestionIndex(savedProgress);
+    })();
+  }, []);
 
   return { data, questionIndex, handleNextQuestion, error };
 };
